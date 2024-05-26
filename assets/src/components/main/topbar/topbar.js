5b823b3eee9d0a0f17bade58a5b8b37f38a8c270
vladikslavus <vladax@list.ru>
2024-05-26 16:31:28 +0300

    Fixed block intersection error in topbarBottom when scrolling up

--- assets/src/components/main/topbar/topbar.js
+++ assets/src/components/main/topbar/topbar.js
@@ -3,17 +3,20 @@ const topbarTop = document.querySelector(".topbar__top");
 const topbarBottom = document.querySelector(".topbar__bottom");
 const nav = document.querySelector(".topbar__bottom-rights");
 const consult = document.querySelector(".topbar__bottom-consult");
-const lgMediaQuery = window.matchMedia('(min-width: 992px)');
+const lgMediaQuery = window.matchMedia("(min-width: 992px)");
 
 const menuBtn = document.querySelector(".topbar__hamburger-wrapper");
 const menuHamburger = document.querySelector(".topbar__hamburger");
 // const content = document.querySelector(".content");
 
 let lastScrollTop = 0;
-window.addEventListener("scroll", function() {
-   const currentScrollTop = window.pageYOffset || document.documentElement.scrollTop;   
+window.addEventListener(
+  "scroll",
+  function () {
+    const currentScrollTop =
+      window.pageYOffset || document.documentElement.scrollTop;
 
-   if (currentScrollTop > lastScrollTop) {
+    if (currentScrollTop > lastScrollTop) {
       // console.log("Down");
       topbarTop.style.display = "none";
       topbarBottom.style.top = "0";
@@ -22,24 +25,31 @@ window.addEventListener("scroll", function() {
         setTimeout(() => {
           consult.style.opacity = "1";
           consult.style.zIndex = "1";
-        }, 300);
-      }      
-   } else if (currentScrollTop < lastScrollTop) {
+        }, 150);
+      }
+    } else if (currentScrollTop < lastScrollTop) {
       // console.log("Up");
       topbarTop.style.display = "block";
-      if(currentScrollTop === 0) {
-        topbarBottom.style.top = `${topbarTop.offsetHeight}px`; // поднимаем на высоту верхнего блока
+      if (currentScrollTop === 0) {
         setTimeout(() => {
-          nav.style.opacity = "1";
-        }, 300);
+          topbarBottom.style.top = `${topbarTop.offsetHeight}px`; // поднимаем на высоту верхнего блока
+        }, 150);
         if (lgMediaQuery.matches === true) {
-          consult.style.opacity = "0";
-          consult.style.zIndex = "-1";
-        }        
+          setTimeout(() => {
+            consult.style.opacity = "0";
+            consult.style.zIndex = "-1";
+
+            setTimeout(() => {
+              nav.style.opacity = "1";
+            }, 150);
+          }, 150);
+        }
       }
-   }
-   lastScrollTop = currentScrollTop <= 0 ? 0 : currentScrollTop;
-}, false);
+    }
+    lastScrollTop = currentScrollTop <= 0 ? 0 : currentScrollTop;
+  },
+  false
+);
 
 // de\activate the menu
 menuBtn.addEventListener("click", (e) => {
